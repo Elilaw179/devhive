2641c67dff2cfbf7747b319a5c2328655ecd4ef0
Elisha Sunday <sirlaw179@gmail.com>
2025-10-11 19:43:15 +0000

    I need this       41dbd9a  to br restored completely

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -88,10 +88,22 @@ export default {
             height: '0',
           },
         },
+        shine: {
+          '0%': {
+            'background-position': '-100% 0',
+          },
+          '50%': {
+            'background-position': '100% 0',
+          },
+          '100%': {
+            'background-position': '100% 0',
+          }
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'shine': 'shine 5s linear infinite',
       },
     },
   },
